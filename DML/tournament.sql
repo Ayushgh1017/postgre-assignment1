196752d8f2e6621ac563ebd06fc042031686b6a0
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 10:12:04 +0530

    Create tournament.sql

---
+++ DML/tournament.sql
@@ -0,0 +1,4 @@
+INSERT INTO tournament (tourid, name, tour_type, country, is_open)
+VALUES (1, 'Tournament 1', 'Type A', 'Country X', true),
+       (2, 'Tournament 2', 'Type B', 'Country Y', false),
+       (3, 'Tournament 3', 'Type C', 'Country Z', true);
