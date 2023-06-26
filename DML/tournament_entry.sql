da7709dfb7f78bc5299662191cca70951732422d
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 10:11:22 +0530

    Create tournament_entry.sql

---
+++ DML/tournament_entry.sql
@@ -0,0 +1,7 @@
+INSERT INTO tournament_entry (memberid, tourid, year)
+VALUES (101, 1, 2022),
+       (102, 1, 2022),
+       (103, 2, 2022),
+       (104, 2, 2022),
+       (105, 3, 2023),
+       (106, 3, 2023);
